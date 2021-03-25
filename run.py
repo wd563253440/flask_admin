7bc88e1a79034cf4ed29b6c317b4ba54702c7977
wd <18986935527@163.com>
2021-03-25 22:31:32 +0800

    修改

--- run.py
+++ run.py
@@ -10,8 +10,6 @@
 -----------------------------------------------
 """
 from app import create_app
-app = create_app()
-
 app, api = create_app(config_name='DEVELOPMENT')
 if __name__ == '__main__':
     app.run(host='127.0.0.1',debug=True)
